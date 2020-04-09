76957ac3d9cee46cb227068e86dff0a8717ff191
Luigi <luigismilzo@live.com>
2020-04-09 16:34:16 +0200

    js event functionality WIP

--- js/main.js
+++ js/main.js
@@ -1,44 +1,13 @@
 $(document).ready( function() {
-    console.log('ok');
+    
+    // REFERENCES
+    var arrowLeft = $('.arrow-left');
+    var arrowRight = $('.arrow-right');
+    var activeImg = $('.img-container img.active');
     
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    // EVENTS
+    arrowRight.click( function() {
+        activeImg.removeClass('active');
+    });
 });
