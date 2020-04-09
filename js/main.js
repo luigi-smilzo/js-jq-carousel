1568bceec4644651be7b96a5083562ad2336c75a
Luigi <luigismilzo@live.com>
2020-04-09 16:41:43 +0200

    Right arrow OK

--- js/main.js
+++ js/main.js
@@ -3,11 +3,20 @@ $(document).ready( function() {
     // REFERENCES
     var arrowLeft = $('.arrow-left');
     var arrowRight = $('.arrow-right');
-    var activeImg = $('.img-container img.active');
+    var firstImg = $('.img-container img.first');
+    var lastImg = $('.img-container img.last');
     
-
+    
     // EVENTS
     arrowRight.click( function() {
-        activeImg.removeClass('active');
+        var activeImg = $('.img-container img.active');
+        
+        if ( activeImg.hasClass('last')) {
+            activeImg.removeClass('active');
+            firstImg.addClass('active');
+        } else {
+            activeImg.removeClass('active');
+            activeImg.next().addClass('active');
+        }
     });
 });
