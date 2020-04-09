ff56cf2758eb70173ba9a0aab6c002748e49a6e0
Luigi <luigismilzo@live.com>
2020-04-09 17:46:00 +0200

    Dots OK

--- js/main.js
+++ js/main.js
@@ -5,30 +5,42 @@ $(document).ready( function() {
     var arrowRight = $('.arrow-right');
     var firstImg = $('.img-container img.first');
     var lastImg = $('.img-container img.last');
+    var firstDot = $('.first.dot');
+    var lastDot = $('.last.dot');
     
     
     // EVENTS
     arrowRight.click( function() {
         var activeImg = $('.img-container img.active');
+        var activeDot = $('.dot.active');
         
         if ( activeImg.hasClass('last')) {
             activeImg.removeClass('active');
             firstImg.addClass('active');
+            activeDot.removeClass('active')
+            firstDot.addClass('active');
         } else {
             activeImg.removeClass('active');
             activeImg.next().addClass('active');
+            activeDot.removeClass('active');
+            activeDot.next().addClass('active');
         }
     });
     
     arrowLeft.click( function() {
         var activeImg = $('.img-container img.active');
+        var activeDot = $('.dot.active');
         
         if ( activeImg.hasClass('first')) {
             activeImg.removeClass('active');
             lastImg.addClass('active');
+            activeDot.removeClass('active')
+            lastDot.addClass('active');
         } else {
             activeImg.removeClass('active');
             activeImg.prev().addClass('active');
+            activeDot.removeClass('active');
+            activeDot.prev().addClass('active');
         }
     });
 });
