fa4d38cbe1a1fa39eaade74c8ee40299487a2c6c
Luigi <luigismilzo@live.com>
2020-04-09 18:11:42 +0200

    prevImg and nextImg functions added

--- js/main.js
+++ js/main.js
@@ -8,39 +8,49 @@ $(document).ready( function() {
     var firstDot = $('.first.dot');
     var lastDot = $('.last.dot');
     
-    
     // EVENTS
     arrowRight.click( function() {
         var activeImg = $('.img-container img.active');
         var activeDot = $('.dot.active');
+
+        nextImg(activeImg, firstImg, activeDot, firstDot);
         
-        if ( activeImg.hasClass('last')) {
-            activeImg.removeClass('active');
-            firstImg.addClass('active');
-            activeDot.removeClass('active')
-            firstDot.addClass('active');
-        } else {
-            activeImg.removeClass('active');
-            activeImg.next().addClass('active');
-            activeDot.removeClass('active');
-            activeDot.next().addClass('active');
-        }
+        
     });
     
     arrowLeft.click( function() {
         var activeImg = $('.img-container img.active');
         var activeDot = $('.dot.active');
-        
-        if ( activeImg.hasClass('first')) {
-            activeImg.removeClass('active');
-            lastImg.addClass('active');
-            activeDot.removeClass('active')
-            lastDot.addClass('active');
-        } else {
-            activeImg.removeClass('active');
-            activeImg.prev().addClass('active');
-            activeDot.removeClass('active');
-            activeDot.prev().addClass('active');
-        }
+
+        prevImg(activeImg, lastImg, activeDot, lastDot);
     });
 });
+
+// FUNCTIONS
+function prevImg (active, last, dot, lastDot) {
+    if ( active.hasClass('first')) {
+        active.removeClass('active');
+        last.addClass('active');
+        dot.removeClass('active')
+        lastDot.addClass('active');
+    } else {
+        active.removeClass('active');
+        active.prev().addClass('active');
+        dot.removeClass('active');
+        dot.prev().addClass('active');
+    }
+}
+
+function nextImg (active, first, dot, firstDot) {
+    if ( active.hasClass('last')) {
+        active.removeClass('active');
+        first.addClass('active');
+        dot.removeClass('active')
+        firstDot.addClass('active');
+    } else {
+        active.removeClass('active');
+        active.next().addClass('active');
+        dot.removeClass('active');
+        dot.next().addClass('active');
+    }
+}
